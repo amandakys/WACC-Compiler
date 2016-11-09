fed2848f6c484dc2d3fe9411c30323c0b8884299
Andi Koh <andikoh@dyn1234-34.wlan.ic.ac.uk>
2016-11-09 22:17:57 +0000

    pushed new ASTs

---
+++ src/AST/PairtypeAST.java
@@ -0,0 +1,22 @@
+package AST;
+
+/**
+ * Created by andikoh on 09/11/2016.
+ */
+public class PairtypeAST extends Node {
+    Node first;
+    Node second;
+
+    public PairtypeAST(Node first, Node second) {
+        super();
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public void check() {
+        first.check();
+        second.check();
+        first.checkType(second);
+    }
+}
