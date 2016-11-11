1635affb12fcdad80f2dbdb535cda2fa64415299
Duy Vu <dtv15@imperial.co.uk>
2016-11-11 10:34:26 +0000

    Adding SyntaxError file

---
+++ src/main/error_handler/SyntaxError.java
@@ -0,0 +1,18 @@
+package main.error_handler;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.misc.Nullable;
+
+public class SyntaxError extends BaseErrorListener {
+    @Override
+    public void syntaxError(@NotNull Recognizer<?, ?> recognizer, @Nullable Object offendingSymbol,
+                            int line, int charPositionLine, @NotNull String msg,
+                            @Nullable RecognitionException e) {
+        System.err.println("line "+ line + ":" + charPositionLine + " : " +
+                msg);
+        System.exit(100);
+    }
+}
